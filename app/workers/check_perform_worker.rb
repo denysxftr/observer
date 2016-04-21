bef8a4772b3ad8f264c04c3100a0b638722bc6fb
Denis Sergienko <olol.toor@gmail.com>
2016-04-21 15:09:45 +0300

    rescue from timeout

--- app/workers/check_perform_worker.rb
+++ app/workers/check_perform_worker.rb
@@ -39,7 +39,7 @@ class CheckPerformWorker
     @status = request.status
     sleep(10) unless @result
     @result
-  rescue Errno::ECONNREFUSED, Errno::ENETDOWN, Errno::ETIMEDOUT, SocketError, IOError, URI::InvalidURIError
+  rescue Errno::ECONNREFUSED, Errno::ENETDOWN, Errno::ETIMEDOUT, SocketError, IOError, URI::InvalidURIError, HTTP::TimeoutError
     @result = false
     @response_time = nil
     sleep(10)
