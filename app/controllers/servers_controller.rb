2d58608ce2b3c18db2752596c4b89aa2adb68e8b
nataliegit <nataliya.bondarenko17@gmail.com>
2016-10-10 12:29:18 +0300

    Created additional functions in servers controller

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -6,11 +6,7 @@ end
 
 post '/server/new' do
   protect!
-  @server = Server.new(
-    name: params[:name],
-    project: !params[:project_id].empty? && Project.find(params[:project_id]),
-    emails: params[:emails] || []
-  )
+  @server = Server.new( server_params )
   @server.save
 
   if @server.valid?
@@ -29,35 +25,33 @@ end
 
 get '/server/:id' do
   protect!
-  @server = Server.find(params[:id])
+  @server = selected_server
   erb :'servers/show'
 end
 
 get '/server/:id/edit' do
   protect!
-  @server = Server.find(params[:id])
+  @server = selected_server
   erb :'servers/edit'
 end
 
 get '/server/:id/last_day' do
   protect!
-  @server = Server.find(params[:id])
+  @server = selected_server
   erb :'servers/last_day'
 end
 
 
 get '/server/:id/last_month' do
   protect!
-  @server = Server.find(params[:id])
+  @server = selected_server
   erb :'servers/last_month'
 end
 
 get '/server/:id/data' do
   protect!
-  server = Server.find(params[:id])
-  states = server.states
-    .where(:created_at.gt => params[:from].to_i.hours.ago, :created_at.lt => params[:to].to_i.hours.ago)
-    .order_by(created_at: 'asc')
+  server = selected_server
+  states = server_states
 
   data = {
     time: states.pluck(:created_at).map { |x| (x.utc + params[:timezone].to_i.hours).strftime('%Y-%m-%d %H:%M:%S') },
@@ -71,10 +65,8 @@ end
 
 get '/server/:id/log_data' do
   protect!
-  server = Server.find(params[:id])
-  states = server.log_states
-    .where(:created_at.gt => params[:from].to_i.days.ago, :created_at.lt => params[:to].to_i.days.ago)
-    .order_by(created_at: 'asc')
+  server = selected_server
+  states = server_states true
 
   data = {
     time: states.pluck(:created_at).map { |x| (x.utc + params[:timezone].to_i.hours).strftime('%Y-%m-%d %H:%M:%S') },
@@ -88,12 +80,8 @@ end
 
 post '/server/:id' do
   protect!
-  @server = Server.find(params[:id])
-  @server.update(
-    name: params[:name],
-    project: !params[:project_id].empty? && Project.find(params[:project_id]),
-    emails: params[:emails] || []
-  )
+  @server = selected_server
+  @server.update( server_params )
   redirect "/server/#{params[:id]}"
 end
 
@@ -103,4 +91,22 @@ post '/servers/:id/delete' do
   server.delete
   session[:success] = 'Server deleted'
   redirect "/"
+end
+
+def selected_server
+  Server.find(params[:id])
+end
+
+def server_states (log = false)
+  (log ? selected_server.log_states : selected_server.states)
+    .where(:created_at.gt => params[:from].to_i.hours.ago, :created_at.lt => params[:to].to_i.hours.ago)
+    .order_by(created_at: 'asc')
+end
+
+def server_params
+  {
+    name: params[:name],
+    project: !params[:project_id].empty? && Project.find(params[:project_id]),
+    emails: params[:emails] || []
+  }
 end
